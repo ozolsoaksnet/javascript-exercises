4b2b2b0d70dffe4628f335666e12ea664e64f120
krisjanis Ozols <ozols.oaksnet@gmail.com>
2019-07-15 17:35:15 +0300

    something already done

--- exercises/warm-up/AddOne/index.js
+++ exercises/warm-up/AddOne/index.js
@@ -4,6 +4,12 @@
  * Array of numbers are passed in the function, your task is to increment each number by one. But without modifying the passed array values.
  */
 
-function addOne(array) {}
+function addOne(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] + 1);
+    }
+    return newArray
+}
 
 module.exports = addOne;
