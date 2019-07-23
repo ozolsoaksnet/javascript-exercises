daf58ea4085cf73a9c3d8d8d727361068e4c0a4a
krisjanis Ozols <ozols.oaksnet@gmail.com>
2019-07-23 14:44:32 +0300

    Anagrams done

--- exercises/easy/Acronym/index.js
+++ exercises/easy/Acronym/index.js
@@ -6,6 +6,18 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input) {}
+function parse(input) {
+    var words, acronym, nextWord;
+
+    words = s.split();
+    acronym = "";
+    index = 0
+    while (index < words.length) {
+        nextWord = words[index];
+        acronym = acronym + nextWord.charAt(0);
+        index = index + 1;
+    }
+    return acronym
+}
 
 module.exports = parse;
